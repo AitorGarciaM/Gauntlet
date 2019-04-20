02c0a7af2a27ea8f0fdc56dfaf6a071cbd689cbc
corbo20 <jgaitor@msn.com>
2019-04-20 20:44:11 +0200

    - Ahora se ignoran las carpetas Debug, Resources y .vs al hacer push
    Signed-off-by: corbo20 <jgaitor@msn.com>

---
+++ Engine 2D/engine2d/Resources/Levels/XML/entities.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="entities" tilewidth="32" tileheight="32" tilecount="350" columns="35">
+ <image source="../../Images/entities.png" width="1120" height="320"/>
+</tileset>
